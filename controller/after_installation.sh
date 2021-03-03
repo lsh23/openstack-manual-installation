872782b8958c353b3eb5fc3a56476378b7714220
lsh23 <shseoul14@gmail.com>
2021-03-03 23:35:59 +0900

    feat: add common variable
    PROVIDER_NETWORK_CIDER
    PROVIDER_NETWORK_GATEWAY

--- controller/after_installation.sh
+++ controller/after_installation.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # this script is based on https://docs.openstack.org/install-guide/launch-instance.html
 
+# load common variable
+. ../common-config
+
 # add the compute node to the cell database
 /bin/sh -c "nova-manage cell_v2 discover_hosts --verbose" nova
 
@@ -12,9 +15,8 @@ openstack network create  --share --external \
 
 ## using gateway, subnet of your provider network
 openstack subnet create --network provider \
-  --allocation-pool start=192.168.0.150,end=192.168.0.254 \
-  --dns-nameserver 8.8.8.8 --gateway 192.168.0.1 \
-  --subnet-range 192.168.0.0/24 provider
+  --dns-nameserver 8.8.8.8 --gateway $PROVIDER_NETWORK_GATEWAY \
+  --subnet-range $PROVIDER_NETWORK_CIDER provider
 
 # make selfservice netowrk
 ## configure any subnet you want. selfservice network is just a your custom network.
